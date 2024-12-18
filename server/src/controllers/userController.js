83ca979ea0150222710fe9d092ed646eeae1ed55
thisaramax20 <thisarathejana20@gmail.com>
2024-12-18 22:21:47 +0530

    login done

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import { UserModel } from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res, next) => {
   const { username, password } = req.body;
@@ -19,4 +20,19 @@ export const registerUser = async (req, res, next) => {
 
 export const loginUser = async (req, res, next) => {
   const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.status(401).json({ message: "User does not exist" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Password not valid" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.status(200).json({ message: "Login Successful", token: token });
 };
